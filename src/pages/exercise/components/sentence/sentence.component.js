6783a8c6abfffee94d88526fc4a6e8c86fc201c6
matluz <matluz994@gmail.com>
2020-05-21 09:39:26 -0300

    add sentence component

---
+++ src/pages/exercise/components/sentence/sentence.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Sentence(props) {
+
+  // const handleChange = (event) => {
+  //   props.setInputText(event.target.value);
+  // }
+
+  return (
+      <span>{props.name}</span>
+  );
+}
--- src/pages/exercise/exercise.page.js
+++ src/pages/exercise/exercise.page.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './exercise.page.css';
 
+import Sentence from './components/sentence/sentence.component'
+
 class Exercise extends React.Component {
   render() {
-    return <div 
-    class="exercise-wrapper">
-      <section class="topbar"></section>
+    return (
+    <div class="exercise-wrapper">
+      <section class="topbar-area"></section>
       <section class="listener-area"></section>
-      <section class="sentence-area"></section>
+      <section class="sentence-area">
+        <Sentence name="hello" />
+      </section>
       <section class="actions-area"></section>
-    </div>;
+    </div>);
   }
 }
 
