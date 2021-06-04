c4551632cee65986080cd0edeb6e2987c2f73c51
matluz <matluz994@gmail.com>
2021-06-04 19:27:07 -0300

    Updates

---
+++ precache-manifest.8be39b4796063a519416101102049b8f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b0bb5b30f69f93110ddce7fbd46265",
+    "url": "/umavoice/index.html"
+  },
+  {
+    "revision": "a084c572d1c6f81770f8",
+    "url": "/umavoice/static/css/main.b51249f3.chunk.css"
+  },
+  {
+    "revision": "947b566e3f9e386d03d4",
+    "url": "/umavoice/static/js/2.86d10a93.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/umavoice/static/js/2.86d10a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a084c572d1c6f81770f8",
+    "url": "/umavoice/static/js/main.6b5ff0c5.chunk.js"
+  },
+  {
+    "revision": "db6af1fb82fd0a26390d",
+    "url": "/umavoice/static/js/runtime-main.36fcb592.js"
+  },
+  {
+    "revision": "607bb7c9aee5ee5158a619fed1df47cf",
+    "url": "/umavoice/static/media/arrow.607bb7c9.svg"
+  },
+  {
+    "revision": "214e768f2584b66e0c8612729fecd87e",
+    "url": "/umavoice/static/media/correct.214e768f.svg"
+  },
+  {
+    "revision": "ede5bf0018d16b7823800823e44e27db",
+    "url": "/umavoice/static/media/incorrect.ede5bf00.svg"
+  },
+  {
+    "revision": "2c38ffbb0447c8d66a0f06ab6dc1fe41",
+    "url": "/umavoice/static/media/listener-one.2c38ffbb.svg"
+  },
+  {
+    "revision": "520d9eb6374db1e03a0a8384dad7c291",
+    "url": "/umavoice/static/media/listening.520d9eb6.svg"
+  },
+  {
+    "revision": "28cb80de3faa26a2601bf5282866d869",
+    "url": "/umavoice/static/media/microphone.28cb80de.svg"
+  },
+  {
+    "revision": "6af5a9142111e0668e52638ad05c267a",
+    "url": "/umavoice/static/media/stop.6af5a914.svg"
+  }
+]);
